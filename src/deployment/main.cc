12676c37c4fe0fc1a06c67b9e90af586a8c01d0b
marsleezm <mars.leezm@gmail.com>
2017-05-18 23:26:54 +0100

    Not blocking forever

--- src/deployment/main.cc
+++ src/deployment/main.cc
@@ -195,12 +195,13 @@ int main(int argc, char** argv) {
 	}
 
 	Connection* batch_connection = multiplexer.NewConnection("scheduler_");
+	Connection* sequencer_connection = multiplexer.NewConnection("sequencer");
   	// Initialize sequencer component and start sequencer thread running.
 
 	assert(argv[2][1] == 'n');
 	int queue_mode = NORMAL_QUEUE;
 
-	Sequencer sequencer(&config, multiplexer.NewConnection("sequencer"), batch_connection,
+	Sequencer sequencer(&config, sequencer_connection, batch_connection,
 		  	  client, storage, queue_mode);
 
 	DeterministicScheduler* scheduler;
@@ -225,6 +226,7 @@ int main(int argc, char** argv) {
 	sequencer.output();
 	delete scheduler;
 	delete batch_connection;
+	delete sequencer_connection;
 	return 0;
 }
 
