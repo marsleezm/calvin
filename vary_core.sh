75fcd32b2f360c813986275c9489e989e18c0fae
marsleezm <mars.leezm@gmail.com>
2017-06-22 11:13:14 +0100

    Increase batch size

--- vary_core.sh
+++ vary_core.sh
@@ -8,11 +8,11 @@ sed -i "s/max_sc =.*/max_sc = ${max_sc}/g" myconfig.conf
 for C in $cores
 do
 	sed -i "s/num_threads =.*/num_threads = $C/g" myconfig.conf
-	sleep 65 && pkill -f deployment &
+	#sleep 65 && pkill -f deployment &
 	./bin/deployment/db 0 mn 0
 	sed -e '/LATENCY/,$d' 0output.txt  > haha
 	tail -n +2 haha > haha2
 	throughput=`awk -F ',' '{sum+=$1;line+=1}END{print sum/line}' haha2` 
-	echo $C ", " $throughput >> result_${max_sc}
+	echo $C , $throughput >> result_${max_sc}
 done
 done
