a4384bafe360c229a130e1430944480127519caf
marsleezm <mars.leezm@gmail.com>
2018-01-13 17:27:25 +0100

    Add val

--- src/applications/microbenchmark.cc
+++ src/applications/microbenchmark.cc
@@ -318,7 +318,7 @@ int Microbenchmark::Execute(StorageManager* storage) const {
 			if(storage->ShouldRead()){
 				Value* index_val = storage->ReadLock(txn->read_write_set(i+indexAccessNum), read_state, false);
 				if(read_state == NORMAL)
-					*index_val = IntToString(StringToInt(*index_val) +  txn->seed()% 100 -50);
+					*index_val = IntToString(NotSoRandomLocalKey(txn->seed(), nparts*index_records, nparts*kDBSize, this_node_id));
 				else
 					return reinterpret_cast<int64>(index_val);
 			}
